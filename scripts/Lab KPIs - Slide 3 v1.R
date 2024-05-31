bb468adfe668440edbd097b47fb078aa7fbba218
iamyannickd <yannickdoungmo@gmail.com>
2024-05-31 09:14:21 +0100

    Update Lab KPIs - Slide 3 v1.R

--- scripts/Lab KPIs - Slide 3 v1.R
+++ scripts/Lab KPIs - Slide 3 v1.R
@@ -236,4 +236,52 @@ AFPtables_gt <-
   
   # Color in gray the table and beautify the formating
   opt_stylize(style = 6, color = 'gray') |>
+  #call that theme
+  #other themes gt_theme_excel()  |>gt_theme_pff() |>
+  gt_theme_excel() |>
+
+  opt_align_table_header(align = "center") |>
+  #reshape the table
+  tab_options(
+    data_row.padding = px(2),
+    summary_row.padding = px(3), # A bit more padding for summaries
+    row_group.padding = px(4)    # And even more for our groups
+  ) |>
+  tab_style(
+    #style = cell_text(weight = "bold"),
+    # Color each country in the group
+    style = cell_fill(color = "lightgray"),
+    locations = cells_row_groups(groups = everything()) 
+  ) 
+
+
+
+
+
+
+
+
+
+
   
+  
+  
+
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
