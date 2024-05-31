584f7d86c6864c0f13e090370e920916d149617d
iamyannickd <yannickdoungmo@gmail.com>
2024-05-31 09:12:23 +0100

    Update Lab KPIs - Slide 3 v1.R

--- scripts/Lab KPIs - Slide 3 v1.R
+++ scripts/Lab KPIs - Slide 3 v1.R
@@ -29,3 +29,68 @@ AFPtables <- DBI::dbGetQuery(AFPdb, "SELECT * FROM POLIOLAB ORDER BY LabName, Ep
 Specify_the_period <- paste0("WEEK 1 - ", 
                              (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 5) |> unique(), ", 2024")
 
+# Analysis of databases =====
+AFPtables_gt <- 
+  AFPtables |>
+  #filter( AFPtables$LabName != "CDC" & year(AFPtables$DateOfOnset) > 2023 ) |>
+  filter( AFPtables$LabName != "CDC") |>
+  #distinct(ICLabID, .keep_all = "TRUE") |>
+  group_by(LabName) |>
+  summarise(workload_by_lab = n()) |>
+  ungroup() |>
+  # samples arrived in good conditions in the lab
+  left_join (
+      AFPtables |>
+        #filter(AFPtables$StoolCondition %in% c("1-Good", "1-Adéquat", "2-Bad", "2-Inadéquat") | is.na(AFPtables$StoolCondition)) |>
+        filter(AFPtables$StoolCondition %in% c("1-Good", "1-Adéquat")) |>
+        #distinct(ICLabID, .keep_all = "TRUE") |>
+        group_by(LabName) |>
+        summarise(Sample_good_cond = n()), 
+      by = "LabName") |>
+      ungroup() |>
+  mutate(Prop_sample_good_cond = round( Sample_good_cond / workload_by_lab * 100, 0) ) |>
+  # total cell culture results =====
+  left_join(
+    AFPtables |>
+      filter(!is.na(AFPtables$FinalCellCultureResult) & !is.nan(AFPtables$FinalCellCultureResult) & !is.null(AFPtables$FinalCellCultureResult)) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(culture_results = n()), 
+    by = "LabName" ) |>
+  # Cell culture results in less than 14 days
+  left_join(
+    AFPtables |>
+      filter(!is.na(AFPtables$FinalCellCultureResult) & !is.nan(AFPtables$FinalCellCultureResult) & !is.null(AFPtables$FinalCellCultureResult) &
+               (AFPtables$DateFinalCellCultureResults - AFPtables$DateStoolReceivedinLab) < 15 & 
+               (AFPtables$DateFinalCellCultureResults - AFPtables$DateStoolReceivedinLab) >= 0 ) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(culture_results_14days = n()), 
+    by = "LabName" ) |>
+  mutate(Prop_culture_results_14days = round(culture_results_14days / culture_results * 100, 0) ) |>
+  # all ITD results
+  left_join(
+    AFPtables |>
+      filter((str_detect(AFPtables$FinalCellCultureResult, "^1") | str_detect(AFPtables$FinalCellCultureResult, "^4"))
+             & !is.nan(AFPtables$FinalITDResult) & !is.null(AFPtables$FinalITDResult)) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(ITD_results = n()), 
+    by = "LabName" ) |>
+  # ITD results in less than 7 days from reception in lab ====
+  left_join(
+    AFPtables |>
+      filter(((str_detect(AFPtables$FinalCellCultureResult, "^1") | str_detect(AFPtables$FinalCellCultureResult, "^4"))
+             & !is.nan(AFPtables$FinalITDResult) & !is.null(AFPtables$FinalITDResult)) 
+            # & between((AFPtables$DateFinalrRTPCRResults - AFPtables$DateIsolateinforITD), ymd_hms(0), ymd_hms(700000)) ) |>
+            & (AFPtables$DateFinalrRTPCRResults - AFPtables$DateIsolateinforITD) >= 0
+            & (as.Date(AFPtables$DateFinalrRTPCRResults) - as.Date(AFPtables$proxy_date_infor_itd)) < 8) 
+
+
+
+
+
+
+
+
+
