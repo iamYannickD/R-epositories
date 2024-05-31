4b011c3a4711cfaf85452070448d7318d7b59674
iamyannickd <yannickdoungmo@gmail.com>
2024-05-31 09:11:16 +0100

    Create Lab KPIs - Slide 3 v1.R

---
+++ scripts/Lab KPIs - Slide 3 v1.R
@@ -0,0 +1,30 @@
+#check directory =====
+getwd()
+
+# Check if the package pacman is installed
+if (!require("pacman")) {install.packages("pacman")} 
+library("pacman")
+
+# Load packages =====
+#RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
+p_load(tidyverse, RODBC,gt, gtExtras, webshot, officer)
+#to convert the web image into an image
+webshot::install_phantomjs()
+
+#Give the path to the AFP database
+path_AFP <- "data/afp_wk18.mdb" 
+
+# Connect to the Microsoft Access database =====
+AFPdb <- DBI::dbConnect(odbc::odbc(), 
+                        .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                              DBQ=", path_AFP))
+
+
+
+
+
+
+
+
+
+
