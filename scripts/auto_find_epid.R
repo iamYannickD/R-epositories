6ad3931f6d049ac9e51c00cc0287b7d556bbbc2c
iamyannickd <yannickdoungmo@gmail.com>
2024-06-03 14:31:54 +0100

    Update auto_find_epid.R

--- scripts/auto_find_epid.R
+++ scripts/auto_find_epid.R
@@ -3,3 +3,12 @@ if (!require("pacman")) {install.packages("pacman")}
 library("pacman")
 
 p_load(tidyverse, readxl)
+
+# Function to search for IDs in all sheets of an Excel file
+search_ids_in_excel <- 
+  function(ids, excel_file) {
+    # Get the names of all sheets in the Excel file
+    sheet_names <- excel_sheets(excel_file)
+    
+    # Create an empty dataframe to store the results
+    results <- data.frame(ID = ids, Found = 0, Sheet = NA, stringsAsFactors = FALSE)
