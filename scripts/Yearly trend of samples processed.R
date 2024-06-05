634004b95da2d19ae6049a65785af13b2d16df4f
iamyannickd <yannickdoungmo@gmail.com>
2024-06-05 11:46:06 +0100

    yearly trend of samples

---
+++ scripts/Yearly trend of samples processed.R
@@ -0,0 +1,24 @@
+# Check if the package pacman is installed
+if (!require("pacman")) {install.packages("pacman")} 
+library("pacman")
+
+# Install and load multiple desired packages at once
+#RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
+# patch work to easily combine and display ggplot graphs
+p_load(tidyverse, RODBC, ggrepel, patchwork)
+
+# import dataset
+workload <- read_csv("../data/data/yr_sample_workloads.csv")
+
+#Give the path to the AFP database
+path_AFP = "../data/dbs/afp_wk21.mdb" 
+path_ES_2024 = "../data/dbs/es_2024.mdb"
+
+# Connect to the Microsoft Access database
+AFPdb <- DBI::dbConnect(odbc::odbc(), 
+                        .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                              DBQ=", path_AFP))
+
+ESdb2024 <- DBI::dbConnect(odbc::odbc(), 
+                           .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                              DBQ=", path_ES_2024))
