048b8db46c4b74a7094377e65545fc2cf2e2048f
iamyannickd <yannickdoungmo@gmail.com>
2024-06-06 12:23:05 +0100

    Update Timeliness cell cultures results.R

--- scripts/Timeliness cell cultures results.R
+++ scripts/Timeliness cell cultures results.R
@@ -5,3 +5,20 @@ library("pacman")
 # Load packages =====
 #RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
 p_load(tidyverse, RODBC,gt, gtExtras, webshot, openxlsx)
+
+#Give the path to the AFP database
+path_AFP <- "../data/dbs/afp_wk21.mdb" 
+
+# Connect to the Microsoft Access database =====
+AFPdb <- DBI::dbConnect(odbc::odbc(), 
+                        .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                              DBQ=", path_AFP))
+# load data in R =====
+# Retrieve all data from the AFP database
+AFPtables <- DBI::dbGetQuery(AFPdb, "SELECT * FROM POLIOLAB ORDER BY LabName, EpidNumber;", stringsAsFactors = FALSE) |>
+  tibble() |>
+  # select samples collected in 2024 only
+  filter(substr(ICLabID, start = 5, stop = 6) == 24 ) 
+
+Specify_the_period <- paste0("WEEK 1 - ", 
+                             (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
