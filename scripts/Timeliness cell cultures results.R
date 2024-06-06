6fbaf63c27b2b738a59834fdb892f72892ba87cc
iamyannickd <yannickdoungmo@gmail.com>
2024-06-06 12:19:46 +0100

    Create Timeliness cell cultures results.R

---
+++ scripts/Timeliness cell cultures results.R
@@ -0,0 +1,7 @@
+# Check if the package pacman is installed
+if (!require("pacman")) {install.packages("pacman")} 
+library("pacman")
+
+# Load packages =====
+#RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
+p_load(tidyverse, RODBC,gt, gtExtras, webshot, openxlsx)
