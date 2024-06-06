4e6def8a3a785c2829d9b5b8fa4b78b25398cb0a
iamyannickd <yannickdoungmo@gmail.com>
2024-06-06 12:23:24 +0100

    Update Timeliness cell cultures results.R

--- scripts/Timeliness cell cultures results.R
+++ scripts/Timeliness cell cultures results.R
@@ -22,3 +22,86 @@ AFPtables <- DBI::dbGetQuery(AFPdb, "SELECT * FROM POLIOLAB ORDER BY LabName, Ep
 
 Specify_the_period <- paste0("WEEK 1 - ", 
                              (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
+
+# Analysis of databases =====
+AFPkpis <- 
+  AFPtables |>
+  #filter( AFPtables$LabName != "CDC" & year(AFPtables$DateOfOnset) > 2023 ) |>
+  filter( AFPtables$LabName != "CDC" & !is.na(FinalCellCultureResult)) |>
+  #distinct(ICLabID, .keep_all = "TRUE") |>
+  group_by(LabName) |>
+  summarise(samples_with_results = n()) |>
+  ungroup() |>
+  left_join (
+    AFPtables |>
+      # starts with 1
+      filter(str_detect(FinalCellCultureResult, "^1")) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(pv_positive = n()), 
+    by = "LabName") |>
+  ungroup() |>
+  mutate(prop_pv_positive = 100 * pv_positive /samples_with_results ) |>
+  left_join (
+    AFPtables |>
+      filter(str_detect(FinalCellCultureResult, "^4")) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(pv_positive_and_npent = n()), 
+    by = "LabName") |>
+  ungroup() |>
+  mutate(prop_pv_positive_and_npent = 100 * pv_positive_and_npent /samples_with_results ) |>
+  left_join (
+    AFPtables |>
+      filter(str_detect(FinalCellCultureResult, "^3")) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(npent = n()), 
+    by = "LabName") |>
+  ungroup() |>
+  mutate(prop_npent = 100 * npent /samples_with_results ) |>
+  left_join (
+    AFPtables |>
+      filter(str_detect(FinalCellCultureResult, "^2")) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(negative = n()), 
+    by = "LabName") |>
+  ungroup() |>
+  mutate(prop_negative = 100 * negative /samples_with_results ) |>
+  left_join(
+    AFPtables |>
+      filter((is.na(AFPtables$FinalCellCultureResult) | is.nan(AFPtables$FinalCellCultureResult) | is.null(AFPtables$FinalCellCultureResult)) 
+             & (as.Date(ymd_hms(AFPtables$DateUpdated)) - ymd(AFPtables$DateStoolReceivedinLab)) >= 15) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(pending_culture_results = n()), 
+    by = "LabName" ) |>
+  ungroup() |>
+  left_join(
+    AFPtables |>
+      filter((is.na(AFPtables$FinalCellCultureResult) | is.nan(AFPtables$FinalCellCultureResult) | is.null(AFPtables$FinalCellCultureResult)) 
+             & ( as.Date(ymd_hms(AFPtables$DateUpdated)) - ymd(AFPtables$DateStoolReceivedinLab)) >= 15 & (as.Date(ymd_hms(AFPtables$DateUpdated)) - ymd(AFPtables$DateStoolReceivedinLab)) <= 30) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(pending_culture_15_30 = n()), 
+    by = "LabName" ) |>
+  ungroup() |>
+  left_join(
+    AFPtables |>
+      filter((is.na(AFPtables$FinalCellCultureResult) | is.nan(AFPtables$FinalCellCultureResult) | is.null(AFPtables$FinalCellCultureResult)) 
+             & (as.Date(ymd_hms(AFPtables$DateUpdated)) - ymd(AFPtables$DateStoolReceivedinLab)) > 30 & (as.Date(ymd_hms(AFPtables$DateUpdated)) - ymd(AFPtables$DateStoolReceivedinLab)) <= 60) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(pending_culture_30_60 = n()), 
+    by = "LabName" ) |>
+  ungroup() |>
+  left_join(
+    AFPtables |>
+      filter((is.na(AFPtables$FinalCellCultureResult) | is.nan(AFPtables$FinalCellCultureResult) | is.null(AFPtables$FinalCellCultureResult)) 
+             & (as.Date(ymd_hms(AFPtables$DateUpdated)) - ymd(AFPtables$DateStoolReceivedinLab)) > 60 ) |>
+      #distinct(ICLabID, .keep_all = "TRUE") |>
+      group_by(LabName) |>
+      summarise(pending_culture_more_60 = n()), 
+    by = "LabName" ) |>
+  ungroup() |>
