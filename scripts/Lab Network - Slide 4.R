4dd4d914771a5c17274052c683e339e737f9b738
iamyannickd <yannickdoungmo@gmail.com>
2024-06-07 13:31:42 +0100

    Update Lab Network - Slide 4.R

--- scripts/Lab Network - Slide 4.R
+++ scripts/Lab Network - Slide 4.R
@@ -13,11 +13,22 @@ polio_labs <- read_csv("../data/data/Geocoordinates of Labs in African region.cs
 # Sequencing laboratories in Afro
 
 # virus count by lab
-virus_by_lab <-
+virusIsol_by_lab <-
   virus_isolation |>
-  group_by(`Virus Isolation`) |>
-  summarise(nber = n()) |>
-  mutate(nber = if_else(`Virus Isolation` == "NIE", nber + 1, nber)) # +1 for Nigeria
+  count(`Virus Isolation`) |>
+  mutate(label_visolation = paste0(`Virus Isolation`, " (", n, ")"))
+
+seq_by_lab <-
+  virus_isolation |>
+  count(`Sequencing Lab`) |>
+  mutate(label_Seq = paste0(`Sequencing Lab`," (", n, ")"))
+
+vec_labs_Seq <-
+  setNames(seq_by_lab$label_Seq, seq_by_lab$`Sequencing Lab`)
+
+# create a named vector with the values of virus by lab
+virus_label <-
+  setNames(virusIsol_by_lab$label_visolation, virusIsol_by_lab$`Virus Isolation`)
 
 # load administrative boundaries =====
 afro_Adm0 <- 
@@ -39,7 +50,8 @@ virus_isolation_map <-
       scale_fill_manual(values = c("ALG" =  "#a6cee3", "SOA" =  "#1f78b4", "GHA" =  "#b2df8a", "SEN" =  "#33a02c", 
                                    "UGA" =  "#fb9a99", "CAE" =  "#ffff99", "CAF" =  "#f768a1", "CIV" =  "cyan", 
                                    "RDC" =  "#ec7014", "ETH" =  "#fdbf6f", "KEN" =  "lightgreen", "MAD" =  "#66bd63",
-                                   "IBD, MDG" =  "#01665e", "ZAM" =  "#cab2d6", "ZIM" =  "#7fcdbb"), na.value = "grey50") +
+                                   "IBD, MDG" =  "#01665e", "ZAM" =  "#cab2d6", "ZIM" =  "#7fcdbb"), na.value = "grey50",
+                        labels = virus_label) +
       
       labs(fill = " ") + # remove the old legend name - Virus Isolation/ ITD Labs
       theme_bw() #+
@@ -53,7 +65,8 @@ sequencing_map <-
           geom_sf(data = afro_cntries, aes(fill = `Sequencing Lab`), color = "black", size = 2) +
           #geom_point(data = polio_labs, aes(x = `X (LONG)` , y = `Y (LAT)`), color = "black", size = 3, shape = 10) +
           scale_fill_manual(values = c("CDC" =  "#377eb8", "NICD" =  "#4daf4a", 
-                                       "IPP" =  "#984ea3", "GHA" =  "#ff7f00"), na.value = "grey50") +
+                                       "IPP" =  "#984ea3", "GHA" =  "#ff7f00"), na.value = "grey50",
+                            labels = vec_labs_Seq) +
           labs(fill = " ") + # remove the old legend name
       theme_bw() #+ 
       #theme(legend.position = "bottom")
