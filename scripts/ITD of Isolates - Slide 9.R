b267c552d71fb3853982d5f45c9827ad1771b1cd
iamyannickd <yannickdoungmo@gmail.com>
2024-06-10 11:12:01 +0100

    Update ITD of Isolates - Slide 9.R

--- scripts/ITD of Isolates - Slide 9.R
+++ scripts/ITD of Isolates - Slide 9.R
@@ -25,6 +25,28 @@ AFPtables <- DBI::dbGetQuery(AFPdb, "SELECT * FROM POLIOLAB ORDER BY LabName, Ep
 Specify_the_period <- paste0("WEEK 1 - ", 
                              (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
 
+# Analysis of databases =====
+#AFPtables_gt <- 
+AFPtables |>
+  filter(LabName != "CDC") |>
+  select(LabName, DateStoolReceivedinLab, StoolCondition, FinalCellCultureResult, DateFinalCellCultureResults,
+         proxy_date_infor_itd, FinalITDResult, DateFinalrRTPCRResults, DateUpdated) |>
+  mutate( FinalCellCultureResult = str_replace_all(FinalCellCultureResult, "Supected", "Suspected") ) |>
+  #distinct(ICLabID, .keep_all = "TRUE") |>
+  group_by(LabName) |>
+  mutate(is_itd = if_else( (FinalCellCultureResult == "1-Suspected Poliovirus" | FinalCellCultureResult == "4-Suspected Poliovirus + NPENT"), 1, 0),
+         time_itd_results_7days = as.numeric(difftime(DateUpdated, proxy_date_infor_itd, units = "days")),
+         ITD_results = sum(is_itd),
+         is_itd_more_7days = if_else( 
+           ( (FinalCellCultureResult == "1-Suspected Poliovirus" | FinalCellCultureResult == "4-Suspected Poliovirus + NPENT") &
+               is.na(FinalITDResult) & time_itd_results_7days >= 8 ), 1, 0),
+         ITD_pending_7days = sum(is_itd_more_7days),
+      ) |>
+  summarize(
+    ITD_results = sum(is_itd, na.rm = TRUE),
+    ITD_pending_7days = sum(is_itd_more_7days, na.rm = TRUE),
+      ) |>
+  dplyr::select(LabName, ITD_results, ITD_pending_7days) 
 
 
 
