cad3e9197d2229f190987cb6b93bff9d77fff2ff
iamyannickd <yannickdoungmo@gmail.com>
2024-06-11 15:40:35 +0100

    Update ITD of Isolates - Slide 9.R

--- scripts/ITD of Isolates - Slide 9.R
+++ scripts/ITD of Isolates - Slide 9.R
@@ -27,9 +27,8 @@ Specify_the_period <- paste0("WEEK 1 - ",
                              (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
 
 # Analysis of databases =====
-#AFPtables_gt <- 
-#tempAFP2 <-
-AFPtables |>
+AFPtables_gt <- 
+  AFPtables |>
   filter(LabName != "CDC") |>
   select(LabName, DateStoolReceivedinLab, StoolCondition, FinalCellCultureResult, DateFinalCellCultureResults,
          proxy_date_infor_itd, FinalITDResult, DateFinalrRTPCRResults, DateUpdated) |>
@@ -154,7 +153,9 @@ AFPtables |>
     "
     )
 
-
+# export 
+AFPtables_gt |> gtsave(filename = "../data/outputs/AFPtables_gt.html", inline_css = TRUE)
+AFPtables_gt |> gtsave("../data/outputs/AFPtables_gt.png")
 
 
 
