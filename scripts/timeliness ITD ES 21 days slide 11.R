1a43d2e64e7524b349954d0662d2690e5914cb87
iamyannickd <yannickdoungmo@gmail.com>
2024-06-14 14:58:04 +0100

    Update timeliness ITD ES 21 days slide 11.R

--- scripts/timeliness ITD ES 21 days slide 11.R
+++ scripts/timeliness ITD ES 21 days slide 11.R
@@ -21,8 +21,8 @@ EStables2024 <- DBI::dbGetQuery(ESdb2024, "SELECT * FROM Environmental ORDER BY 
   mutate(Labname = str_replace_all(Labname, c("ENTEBBE" = "UGA", "GHANA" = "GHA", "INRB" = "RDC", "IPD SEN" = "SEN",
                                               "IPM, MAD" = "MAD", "IPM,MAD" = "MAD", "KEMRI" = "KEN", "IBD, Nigeria" = "IBD",
                                               "MDG, Nigeria" = "MDG", "ZAM UTH" = "ZAM", "ZAM-UTH" = "ZAM")),
-         date_result_to_lab = if_else(is.na(Dateresultstolab), 
-                                      Datefinalcultureresult, Dateresultstolab)
+         date_result_to_lab = coalesce(Dateresultstolab, Datefinalcultureresult),
+         date_itd_result = coalesce(DateFinalCombinedResult, DatefinalResultReported)
   )
 
 #Specify_the_period <- paste0("WEEK 1 - ", (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
@@ -43,12 +43,12 @@ EStables2024 |>
     
     
     ITD_results_21days = sum( (str_detect(Finalcellcultureresult, "^1") | str_detect(Finalcellcultureresult, "^4")) & 
-                                !is.na(FinalcombinedrRTPCRresults) & (DateFinalCombinedResult - Datesampleinlab) < 22 & 
-                                (DateFinalCombinedResult - Datesampleinlab) >= 0, na.rm = TRUE),
+                                !is.na(FinalcombinedrRTPCRresults) & (date_itd_result - Datesampleinlab) < 22 & 
+                                (date_itd_result - Datesampleinlab) >= 0, na.rm = TRUE),
     ITD_results_21days_positive = sum( (str_detect(Finalcellcultureresult, "^1") | str_detect(Finalcellcultureresult, "^4")) & 
                                    (str_detect(FinalcombinedrRTPCRresults, "Discordant") | str_detect(FinalcombinedrRTPCRresults, "PV2")) &
-                                !is.na(FinalcombinedrRTPCRresults) & (DateFinalCombinedResult - Datesampleinlab) < 22 & 
-                                (DateFinalCombinedResult - Datesampleinlab) >= 0, na.rm = TRUE),
+                                !is.na(FinalcombinedrRTPCRresults) & (date_itd_result - Datesampleinlab) < 22 & 
+                                (date_itd_result - Datesampleinlab) >= 0, na.rm = TRUE),
     Prop_ITD_21days = round(ITD_results_21days / ITD_results * 100, 0),
     Prop_ITD_21days_positive = round(ITD_results_21days_positive / ITD_results_positive * 100, 0)
     ) |>
