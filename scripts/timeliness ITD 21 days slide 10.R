0d91d4520a7bf0b2e4dde696d0d374df908ae99e
iamyannickd <yannickdoungmo@gmail.com>
2024-06-15 09:24:01 +0100

    removed DJI and SOM from KEMRI

--- scripts/timeliness ITD 21 days slide 10.R
+++ scripts/timeliness ITD 21 days slide 10.R
@@ -29,6 +29,7 @@ Specify_the_period <- paste0("WEEK 1 - ",
 #AFPtables_gt <- 
 AFPtables |>
   filter(LabName != "CDC") |>
+  #filter(substr(EpidNumber, start = 1, stop = 3) != c("DJI", "SOM") ) |> #remove somalia and djibouti
   select(LabName, DateStoolReceivedinLab, StoolCondition, FinalCellCultureResult, DateFinalCellCultureResults,
          proxy_date_infor_itd, FinalITDResult, DateFinalrRTPCRResults) |>
   mutate( FinalCellCultureResult = str_replace_all(FinalCellCultureResult, "Supected", "Suspected") ) |>
