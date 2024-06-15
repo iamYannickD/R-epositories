5099a771f3a108aca7f2a9e9411604cda7c45925
iamyannickd <yannickdoungmo@gmail.com>
2024-06-15 12:49:15 +0100

    Create timeliness by Country - ITD AFP 35 days slide 13.R

---
+++ scripts/timeliness by Country - ITD AFP 35 days slide 13.R
@@ -0,0 +1,28 @@
+# Check if the package pacman is installed
+if (!require("pacman")) {install.packages("pacman")} 
+library("pacman")
+
+# Load packages =====
+#RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
+p_load(tidyverse, RODBC,gt, gtExtras, webshot, officer)
+
+#Give the path to the AFP database
+path_AFP <- "../data/dbs/afp_wk35.mdb" 
+
+# Connect to the Microsoft Access database =====
+AFPdb <- DBI::dbConnect(odbc::odbc(), 
+                        .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                              DBQ=", path_AFP))
+# load data in R =====
+# Retrieve all data from the AFP database
+AFPtables <- DBI::dbGetQuery(AFPdb, "SELECT * FROM POLIOLAB ORDER BY LabName, EpidNumber;", stringsAsFactors = FALSE) |>
+  tibble() |>  mutate(
+    proxy_date_infor_itd = coalesce(DateIsolateinforITD, DateLarmIsolateRec, DateRarmIsolateSentforITD),
+    proxy_date_itd_result = coalesce(DateFinalrRTPCRResults, DateFinalResultsSentReflabEPI)
+    
+  ) |>
+  # select samples collected in 2024 only
+  filter(substr(ICLabID, start = 5, stop = 6) == 24 )
+
+Specify_the_period <- paste0("WEEK 1 - ", 
+                             (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
