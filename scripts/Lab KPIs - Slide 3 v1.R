e5895ae91570c0f164e9fcdcaa44ec54ade86f44
iamyannickd <yannickdoungmo@gmail.com>
2024-06-19 21:20:02 +0100

    Update Lab KPIs - Slide 3 v1.R

--- scripts/Lab KPIs - Slide 3 v1.R
+++ scripts/Lab KPIs - Slide 3 v1.R
@@ -7,7 +7,7 @@ library("pacman")
 p_load(tidyverse, RODBC,gt, gtExtras, webshot, officer)
 
 #Give the path to the AFP database
-path_AFP <- "../data/dbs/afp_wk21.mdb" 
+path_AFP <- "../data/dbs/wk_24/testAFP.mdb" 
 
 # Connect to the Microsoft Access database =====
 AFPdb <- DBI::dbConnect(odbc::odbc(), 
@@ -16,16 +16,13 @@ AFPdb <- DBI::dbConnect(odbc::odbc(),
 # load data in R =====
 # Retrieve all data from the AFP database
 AFPtables <- DBI::dbGetQuery(AFPdb, "SELECT * FROM POLIOLAB ORDER BY LabName, EpidNumber;", stringsAsFactors = FALSE) |>
-  tibble() |>  mutate(proxy_date_infor_itd = if_else(is.na(DateIsolateinforITD),
-                                if_else(is.na(DateLarmIsolateRec), DateRarmIsolateSentforITD, DateLarmIsolateRec),
-                                DateIsolateinforITD
-                               )
-         ) |>
+  tibble() |>  mutate(proxy_date_infor_itd = coalesce(DateIsolateinforITD, DateLarmIsolateRec, DateRarmIsolateSentforITD)
+                   ) |>
   # select samples collected in 2024 only
   filter(substr(ICLabID, start = 5, stop = 6) == 24 )
 
-Specify_the_period <- paste0("WEEK 1 - ", 
-                             (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
+Specify_the_period <- paste0("WEEK 1 - 24, 2024")   # , 
+                        #     (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
 
 # Analysis of databases =====
 #AFPtables_gt <- 
