a866783565d1fd9d51dedb4816b0d43504eac2bf
iamyannickd <yannickdoungmo@gmail.com>
2024-07-17 14:14:39 +0100

    Update recap_analysis_by_quarter.R

--- scripts/desk_review/recap_analysis_by_quarter.R
+++ scripts/desk_review/recap_analysis_by_quarter.R
@@ -10,4 +10,57 @@ library("pacman")
 p_load(tidyverse, sf, geojsonsf, ggspatial, ggrepel)
 
 #load data
-load_es_sites <- read_csv("../data/data_dr/es_sites/ES_site_analysis_apr_jun_2024-07-09.csv") |>
+
+# Classification of countries based on their level of risk
+Very_high_risk <- c("Chad", "Democratic Republic of Congo", "Madagascar", "Mozambique", "Niger", "Nigeria")
+
+High_risk	<- c("Algeria", "Angola", "Benin", "Burkina Faso", "Cameroon", "Central African Republic", "Côte d’Ivoire", 
+               "Ethiopia", "Kenya", "Malawi", "Mali", "Zambia")
+
+Medium_high_risk <- c("Burundi", "Republic of Congo", "Equatorial Guinea", "Eritrea", "Gabon", "Gambia", "Ghana", "Guinea", "Guinea Bissau", 
+                      "Liberia", "Mauritania", "Namibia", "Rwanda", "Senegal", "Sierra Leone", "South Sudan", "Tanzania", 
+                      "Togo", "Uganda", "Zimbabwe")
+Medium_Risk <- c("")
+
+# Create a tibble containing values in the list provided and their risk category
+risk_level_by_country <- tibble(
+  risk_level = c(rep("Very High Risk", length(Very_high_risk)),
+                 rep("High Risk", length(High_risk)),
+                 rep("Medium High Risk", length(Medium_high_risk)),
+                 rep("Medium Risk", length(Medium_Risk))),
+  country = c(Very_high_risk, High_risk, Medium_high_risk, Medium_Risk) |> str_to_upper()
+) |>
+  mutate(
+    country = str_replace_all(country, c("CHAD" = "TCHAD",
+                                         "UNITED REPUBLIC OF TANZANIA" = "TANZANIA",
+                                         "CÔTE D’IVOIRE" = "COTE D'IVOIRE",
+                                         "GUINEA"  = "GUINEE",
+                                         "MAURITANIA" = "MAURITANIE", 
+                                         "EQUATORIAL GUINEE" = "EQUATORIAL GUINEA" ))
+  )
+
+#load_es_sites <- 
+  read_csv("../data/data_dr/es_sites/ES_site_analysis_jan_jun_2024-07-09.csv") |>
+  dplyr::select(Countryname, EV_isolation_Rate_3m, EV_isolation_Rate_6m) |>
+    left_join(y = risk_level_by_country, by = c("Countryname" = "country")) |>
+    filter(!is.na(risk_level)) |>
+    group_by(Countryname, risk_level) |>
+    mutate(
+      country_3m = if_else(!is.na(EV_isolation_Rate_3m), 1, 0),
+      country_3m_50ev = if_else( (!is.na(EV_isolation_Rate_3m) & EV_isolation_Rate_3m  >= 50) , 1, 0),
+      country_3m_80ev = if_else( (!is.na(EV_isolation_Rate_3m) & EV_isolation_Rate_3m  >= 80), 1, 0),
+      
+      country_6m = if_else(!is.na(EV_isolation_Rate_6m), 1, 0),
+      country_6m_50ev = if_else( (!is.na(EV_isolation_Rate_6m) & EV_isolation_Rate_6m  >= 50) , 1, 0),
+      country_6m_80ev = if_else( (!is.na(EV_isolation_Rate_6m) & EV_isolation_Rate_6m  >= 80), 1, 0),
+    ) |>
+    summarise(
+      `%Q1countries >= 50` = sum(country_3m_50ev, na.rm = TRUE) / sum(country_3m, na.rm = TRUE),
+      `%Q2countries >= 50` = sum(country_6m_50ev, na.rm = TRUE) / sum(country_6m, na.rm = TRUE),
+      
+      `%Q1countries >= 80` = sum(country_3m_80ev, na.rm = TRUE) / sum(country_3m, na.rm = TRUE),
+      `%Q2countries >= 80` = sum(country_6m_80ev, na.rm = TRUE) / sum(country_6m, na.rm = TRUE)
+    )
+  
+  
+
