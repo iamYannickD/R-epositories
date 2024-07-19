1f1465f9a4808a7f1c20c30ce0e09b55bda071d0
iamyannickd <yannickdoungmo@gmail.com>
2024-07-19 14:13:36 +0100

    Update Slide 1 - workload.R

--- scripts/lab_assesment/Slide 1 - workload.R
+++ scripts/lab_assesment/Slide 1 - workload.R
@@ -54,7 +54,7 @@ Specify_the_period <- paste0("WEEK 1 - ",
   geom_text(aes(x = Month, y = afp_workload_by_lab, label = afp_workload_by_lab), size = 3, fontface = "bold", position = position_stack(vjust = 0.5) ) + #vjust = 0.5
     #scale_colour_brewer(palette = "PiYG") +
     scale_fill_brewer(palette = "Pastel1") +
-  labs(x = " ", y = "Number of AFP Samples Processed", title = "Samples analyzed by Month and by Countries", fill = "Countries" ) +
+  labs(x = " ", y = "Number of AFP Samples Processed", title = paste0("Samples processed by Month and by Countries in ", labname), fill = "Countries" ) +
   theme_classic() + 
   theme(
     axis.text = element_text(face = "bold", size = 10, color = "black"),
