fa8faa37afaaaca33dff340ac83835941998801a
iamyannickd <yannickdoungmo@gmail.com>
2024-07-21 00:12:36 +0100

    Update Slide 3 - AFP Primary Isolation KPIs.R

--- scripts/lab_assesment/Slide 3 - AFP Primary Isolation KPIs.R
+++ scripts/lab_assesment/Slide 3 - AFP Primary Isolation KPIs.R
@@ -26,7 +26,7 @@ Specify_the_period <- paste0("WEEK 1 - ",
                              (epiweek(as.Date(ymd(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
 
 # Analysis of databases =====
-AFPkpis <-
+#AFPkpis <-
   AFPtables |>
   filter(LabName == labname) |>
   mutate( FinalCellCultureResult = str_replace_all(FinalCellCultureResult, "Supected", "Suspected"),
@@ -60,48 +60,72 @@ AFPkpis <-
          is_pending_culture_more_60 = if_else(is.na(FinalCellCultureResult) & days_to_results >= 60, 1, 0)
   ) |> 
   summarise(
-    samples_with_results = sum(is_result, na.rm = TRUE),
+    nb_samples_with_results = sum(is_result, na.rm = TRUE),
     
-    culture_results_14days = sum(is_culture_results_14days, na.rm = TRUE),
-    Prop_culture_results_14days = 100 * culture_results_14days / samples_with_results,
+    nb_culture_results_14days = sum(is_culture_results_14days, na.rm = TRUE),
+    Prop_culture_results_14days = 100 * nb_culture_results_14days / nb_samples_with_results,
     
-    pv_positive = sum(is_pv_positive, na.rm = TRUE),
-    prop_pv_positive = 100 * pv_positive / samples_with_results,
+    nb_pv_positive = sum(is_pv_positive, na.rm = TRUE),
+    prop_pv_positive = 100 * nb_pv_positive / nb_samples_with_results,
     
-    pv_positive_and_npent = sum(is_pv_positive_and_npent, na.rm = TRUE),
-    prop_pv_positive_and_npent = 100 * pv_positive_and_npent / samples_with_results,
+    nb_pv_positive_and_npent = sum(is_pv_positive_and_npent, na.rm = TRUE),
+    prop_pv_positive_and_npent = 100 * nb_pv_positive_and_npent / nb_samples_with_results,
     
-    npent = sum(is_npent, na.rm = TRUE), 
-    prop_npent = 100 * npent / samples_with_results,
+    nb_npent = sum(is_npent, na.rm = TRUE), 
+    prop_npent = 100 * nb_npent / nb_samples_with_results,
     
-    pv_negative = sum(is_pv_negative, na.rm = TRUE),
-    prop_negative = 100 * pv_negative / samples_with_results,
+    nb_pv_negative = sum(is_pv_negative, na.rm = TRUE),
+    prop_negative = 100 * nb_pv_negative / nb_samples_with_results,
     
-    pending_culture_results = sum(is_pending_culture_results),
-    pending_culture_15_30 = sum(is_pending_culture_15_30),
-    pending_culture_30_60 = sum(is_pending_culture_30_60),
-    pending_culture_more_60 = sum(is_pending_culture_more_60)
+    nb_pending_culture_results = sum(is_pending_culture_results),
+    nb_pending_culture_15_30 = sum(is_pending_culture_15_30),
+    nb_pending_culture_30_60 = sum(is_pending_culture_30_60),
+    nb_pending_culture_more_60 = sum(is_pending_culture_more_60)
   ) |>
   ungroup() |>
   gt() |>
   #edit some columns names
   cols_label(
-    "samples_with_results" = "# of Specimens with Results",
-    "culture_results_14days" = "# of culture results in 14 days",
+    "nb_samples_with_results" = "# of Specimens with Results",
+    "nb_culture_results_14days" = "# of culture results in 14 days",
     "Prop_culture_results_14days" = "Prop cell cultures results 14 days",
-    "pv_positive" = "Culture +ve for PV",
+    "nb_pv_positive" = "Culture +ve for PV",
     "prop_pv_positive" = "Prop Culture +ve for PV",
-    "pv_positive_and_npent" = "Culture +ve for PV & NPEV",
+    "nb_pv_positive_and_npent" = "Culture +ve for PV & NPEV",
     "prop_pv_positive_and_npent" = "Prop Culture +ve for PV & NPEV",
-    "npent" = "NPEV",
+    "nb_npent" = "NPEV",
     "prop_npent" = "Prop NPEV",
-    "pv_negative" = "Negative",
+    "nb_pv_negative" = "Negative",
     "prop_negative" = "Prop Negative",
-    "pending_culture_results" = "Total Pending",
-    "pending_culture_15_30" = "15 - 30 days",
-    "pending_culture_30_60" = "31 - 60 days",
-    "pending_culture_more_60" = "> 60 days"
+    "nb_pending_culture_results" = "Total Pending",
+    "nb_pending_culture_15_30" = "15 - 30 days",
+    "nb_pending_culture_30_60" = "31 - 60 days",
+    "nb_pending_culture_more_60" = "> 60 days"
   ) |>
+  # insert a summary for proportions
+  grand_summary_rows(
+    columns = starts_with("Prop"),
+    #missing_text = "-",
+    fns = list(
+      "AVG" = ~ mean(.x, na.rm = TRUE)
+    ) )  |>
+  # fmt_percent(
+  #   columns = starts_with("Prop"),
+  #   rows = grand_summary_rows,
+  #   decimals = 1 ) 
+  grand_summary_rows(
+    columns = starts_with("nb_"),
+    #missing_text = "-",
+    fns = list(
+      "TOTAL" = ~ sum(.x, na.rm = TRUE)
+    ) )  |>
+  # customize the summary line
+  tab_style(
+    style = list(
+      cell_text(weight = "bold", size = px(14))
+    ),
+    locations = cells_grand_summary()
+  )  |>
   #center the values in the defined columns
   cols_align(
     align = "center",
@@ -139,12 +163,12 @@ AFPkpis <-
   ) |>
   # add percentage in cells
   fmt_number(
-    columns = c(1:16),
+    columns = starts_with("nb_"),
     decimals = 0,
     pattern = "{x}"
   ) |>
   fmt_number(
-    columns = c(Prop_culture_results_14days, prop_pv_positive, prop_pv_positive_and_npent, prop_npent, prop_negative ),
+    columns = starts_with("Prop"),
     decimals = 1,
     pattern = "{x} %"
   ) |>
@@ -153,32 +177,29 @@ AFPkpis <-
 tab_style(
   style = cell_fill(color = "pink"),
   locations = cells_body(
-    columns = pending_culture_results,
-    rows = pending_culture_results >= 15)
+    columns = nb_pending_culture_results,
+    rows = nb_pending_culture_results >= 15)
 )  |>
   tab_style(
     style = cell_fill(color = "red2"),
     locations = cells_body(
-      columns = pending_culture_results,
-      rows = pending_culture_results >= 31)
+      columns = nb_pending_culture_results,
+      rows = nb_pending_culture_results >= 31)
   ) |> 
   tab_style(
     style = cell_fill(color = "red4"),
     locations = cells_body(
-      columns = pending_culture_results,
-      rows = pending_culture_results >= 61)
+      columns = nb_pending_culture_results,
+      rows = nb_pending_culture_results >= 61)
   ) |>
   data_color(
     #method = "numeric",
-    columns = pending_culture_results,
-    rows = pending_culture_results >= 15,
+    columns = nb_pending_culture_results,
+    rows = nb_pending_culture_results >= 15,
     palette = "ggsci::red_material"
   )  |>
   # Color in gray the table and beautify the formating
-  opt_stylize(style = 6, color = 'gray') |>
-  #call that theme
-  #other themes 
-  #gt_theme_excel()  |> gt_theme_pff() |> 
+  #opt_stylize(style = 6, color = 'gray') |> #remove style on lines
   gt_theme_538() |>
   
   opt_align_table_header(align = "center") |>
