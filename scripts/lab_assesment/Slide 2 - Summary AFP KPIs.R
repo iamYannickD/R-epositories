5e55bfccc9f41ecc69a410363f6dca946164dfc4
iamyannickd <yannickdoungmo@gmail.com>
2024-07-21 20:59:13 +0100

    Update Slide 2 - Summary AFP KPIs.R

--- scripts/lab_assesment/Slide 2 - Summary AFP KPIs.R
+++ scripts/lab_assesment/Slide 2 - Summary AFP KPIs.R
@@ -224,9 +224,7 @@ tab_style(
   ) |>
   
   # Color in gray the table and beautify the formating
-  opt_stylize(style = 6, color = 'gray') |>
-  #call that theme
-  #other themes gt_theme_excel()  |>gt_theme_pff() |>
+  #opt_stylize(style = 6, color = 'gray') |>
   gt_theme_538()|>
   
   opt_align_table_header(align = "center") |>
