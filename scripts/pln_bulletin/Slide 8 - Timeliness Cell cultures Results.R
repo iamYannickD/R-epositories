7547124c2d06981354d1c63506e0b58f6a09d490
iamyannickd <yannickdoungmo@gmail.com>
2024-07-23 12:13:20 +0100

    Update Slide 8 - Timeliness Cell cultures Results.R

--- scripts/pln_bulletin/Slide 8 - Timeliness Cell cultures Results.R
+++ scripts/pln_bulletin/Slide 8 - Timeliness Cell cultures Results.R
@@ -7,7 +7,7 @@ library("pacman")
 p_load(tidyverse, RODBC,gt, gtExtras, webshot, openxlsx)
 
 #Give the path to the AFP database
-path_AFP <- "../data/dbs/wk_24/afp_wk_24.mdb" 
+path_AFP <- "../data/dbs/AFP_160724.mdb" 
 dateDBreceived <- "2024-06-18 14:24:05"
 
 # Connect to the Microsoft Access database =====
@@ -58,48 +58,54 @@ AFPkpis <-
          is_pending_culture_more_60 = if_else(is.na(FinalCellCultureResult) & days_to_results >= 60, 1, 0)
              ) |> 
   summarise(
-         samples_with_results = sum(is_result, na.rm = TRUE),
+         nb_samples_with_results = sum(is_result, na.rm = TRUE),
          
-         culture_results_14days = sum(is_culture_results_14days, na.rm = TRUE),
-         Prop_culture_results_14days = 100 * culture_results_14days / samples_with_results,
+         nb_culture_results_14days = sum(is_culture_results_14days, na.rm = TRUE),
+         Prop_culture_results_14days = 100 * nb_culture_results_14days / nb_samples_with_results,
          
-         pv_positive = sum(is_pv_positive, na.rm = TRUE),
-         prop_pv_positive = 100 * pv_positive / samples_with_results,
+         nb_pv_positive = sum(is_pv_positive, na.rm = TRUE),
+         prop_pv_positive = 100 * nb_pv_positive / nb_samples_with_results,
          
-         pv_positive_and_npent = sum(is_pv_positive_and_npent, na.rm = TRUE),
-         prop_pv_positive_and_npent = 100 * pv_positive_and_npent / samples_with_results,
+         nb_pv_positive_and_npent = sum(is_pv_positive_and_npent, na.rm = TRUE),
+         prop_pv_positive_and_npent = 100 * nb_pv_positive_and_npent / nb_samples_with_results,
          
-         npent = sum(is_npent, na.rm = TRUE), 
-         prop_npent = 100 * npent / samples_with_results,
+         nb_npent = sum(is_npent, na.rm = TRUE), 
+         prop_npent = 100 * nb_npent / nb_samples_with_results,
          
-         pv_negative = sum(is_pv_negative, na.rm = TRUE),
-         prop_negative = 100 * pv_negative / samples_with_results,
+         nb_pv_negative = sum(is_pv_negative, na.rm = TRUE),
+         prop_negative = 100 * nb_pv_negative / nb_samples_with_results,
          
-         pending_culture_results = sum(is_pending_culture_results),
-         pending_culture_15_30 = sum(is_pending_culture_15_30),
-         pending_culture_30_60 = sum(is_pending_culture_30_60),
-         pending_culture_more_60 = sum(is_pending_culture_more_60)
+         nb_pending_culture_results = sum(is_pending_culture_results),
+         nb_pending_culture_15_30 = sum(is_pending_culture_15_30),
+         nb_pending_culture_30_60 = sum(is_pending_culture_30_60),
+         nb_pending_culture_more_60 = sum(is_pending_culture_more_60)
          ) |>
-  ungroup() |>
+  ungroup() |> 
   gt() |>
   #edit some columns names
   cols_label(
-    "samples_with_results" = "# of Specimens with Results",
-    "culture_results_14days" = "# of culture results in 14 days",
+    "nb_samples_with_results" = "# of Specimens with Results",
+    "nb_culture_results_14days" = "# of culture results in 14 days",
     "Prop_culture_results_14days" = "Prop cell cultures results 14 days",
-    "pv_positive" = "Culture +ve for PV",
+    "nb_pv_positive" = "Culture +ve for PV",
     "prop_pv_positive" = "Prop Culture +ve for PV",
-    "pv_positive_and_npent" = "Culture +ve for PV & NPEV",
+    "nb_pv_positive_and_npent" = "Culture +ve for PV & NPEV",
     "prop_pv_positive_and_npent" = "Prop Culture +ve for PV & NPEV",
-    "npent" = "NPEV",
+    "nb_npent" = "NPEV",
     "prop_npent" = "Prop NPEV",
-    "pv_negative" = "Negative",
+    "nb_pv_negative" = "Negative",
     "prop_negative" = "Prop Negative",
-    "pending_culture_results" = "Total Pending",
-    "pending_culture_15_30" = "15 - 30 days",
-    "pending_culture_30_60" = "31 - 60 days",
-    "pending_culture_more_60" = "> 60 days"
+    "nb_pending_culture_results" = "Total Pending",
+    "nb_pending_culture_15_30" = "15 - 30 days",
+    "nb_pending_culture_30_60" = "31 - 60 days",
+    "nb_pending_culture_more_60" = "> 60 days"
   ) |>
+  grand_summary_rows(
+    columns = starts_with("nb_"),
+    #missing_text = "-",
+    fns = list(
+      "TOTAL" = ~ sum(.x, na.rm = TRUE)
+    ) ) |>
   #center the values in the defined columns
   cols_align(
     align = "center",
@@ -142,7 +148,7 @@ AFPkpis <-
     pattern = "{x}"
   ) |>
   fmt_number(
-    columns = c(Prop_culture_results_14days, prop_pv_positive, prop_pv_positive_and_npent, prop_npent, prop_negative ),
+    columns = starts_with("Prop"),
     decimals = 1,
     pattern = "{x} %"
   ) |>
@@ -151,25 +157,25 @@ AFPkpis <-
 tab_style(
   style = cell_fill(color = "pink"),
   locations = cells_body(
-    columns = pending_culture_results,
-    rows = pending_culture_results >= 15)
+    columns = nb_pending_culture_results,
+    rows = nb_pending_culture_results >= 15)
 )  |>
   tab_style(
     style = cell_fill(color = "red2"),
     locations = cells_body(
-      columns = pending_culture_results,
-      rows = pending_culture_results >= 31)
+      columns = nb_pending_culture_results,
+      rows = nb_pending_culture_results >= 31)
   ) |> 
   tab_style(
     style = cell_fill(color = "red4"),
     locations = cells_body(
-      columns = pending_culture_results,
-      rows = pending_culture_results >= 61)
+      columns = nb_pending_culture_results,
+      rows = nb_pending_culture_results >= 61)
   ) |>
   data_color(
     #method = "numeric",
-    columns = pending_culture_results,
-    rows = pending_culture_results >= 15,
+    columns = nb_pending_culture_results,
+    rows = nb_pending_culture_results >= 15,
     palette = "ggsci::red_material"
   )  |>
   # Color in gray the table and beautify the formating
@@ -208,28 +214,10 @@ tab_style(
     "
   )
 
+AFPkpis
 
 # export my table
 gtsave(AFPkpis, "../data/outputs/AFPKpis.html")
-# Convert HTML to PNG
-webshot::webshot("../data/output/AFPtables.html", "../data/output/AFPKpis.png")
-
-# convert html to excel file
-#write.xlsx(AFPkpis_df, "output/AFPKpis.xlsx")
-
-
-# open the table of our presentation change link ppt
-Pres_ppt <- read_pptx(path = "../data/AFRO polio labs bulletin week 1-18_2024.pptx")
-
-# Insert the image in a new slide
-#Pres_ppt <- ph_with(on_slide(Pres_ppt, index = 3), external_img("output/AFPtables.png"), location = ph_location_fullsize())
-
-# add the table in the 4th slide of the presentation
-Pres_ppt <- ph_with(on_slide(Pres_ppt, index = 9), external_img("output/AFPKpis.png"), 
-                    location = ph_location(left = 0.2, top = 1, width = 13, height = 8))
-
-# Save the updated presentation
-print(Pres_ppt, target = "data/AFRO polio labs bulletin week 1-18_2024.pptx")  
 
 
 
