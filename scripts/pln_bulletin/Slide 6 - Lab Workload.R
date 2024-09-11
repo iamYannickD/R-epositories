df1fa22a66d25202673d65c5f3057c4558e1ab6a
iamyannickd <yannickdoungmo@gmail.com>
2024-09-11 13:54:04 +0100

    Update Slide 6 - Lab Workload.R

--- scripts/pln_bulletin/Slide 6 - Lab Workload.R
+++ scripts/pln_bulletin/Slide 6 - Lab Workload.R
@@ -11,8 +11,8 @@ library("pacman")
 p_load(tidyverse, RODBC, patchwork)
 
 #Give the path to the AFP database
-path_AFP = "../data/dbs/AFP_160724.mdb" 
-path_ES_2024 = "../data/dbs/ES_160724.mdb"
+path_AFP = "../data/dbs/AFP_09092024.mdb" 
+path_ES_2024 = "../data/dbs/ES_2024_09092024.mdb"
 
 # Connect to the Microsoft Access database
 AFPdb <- DBI::dbConnect(odbc::odbc(), 
@@ -45,7 +45,7 @@ AFP_plot <-
   group_by(LabName) |>
   summarise(afp_workload_by_lab = n()) |>
   ungroup() |>
-  #summarise(total_workload = sum(workload_by_lab)) |>
+  #summarise(total_workload = sum(afp_workload_by_lab)) # summary
   ggplot() +
   geom_bar(aes(x = LabName, y = afp_workload_by_lab, fill = "darkblue"), fill = "darkblue", stat = "identity") + 
   geom_text(aes(x = LabName, y = afp_workload_by_lab, label = afp_workload_by_lab), size = 3.5, fontface = "bold", vjust = -0.5) +
@@ -72,6 +72,7 @@ ES_plot <-
   group_by(Labname) |> #total ES samples
   summarise(es_workload_by_lab = n()) |>
   ungroup() |>
+  #summarise(total_workload = sum(es_workload_by_lab)) #summary
   ggplot() +
   geom_bar(aes(x = Labname, y = es_workload_by_lab, fill = "orange"), fill = "orange", stat = "identity") + 
   geom_text(aes(x = Labname, y = es_workload_by_lab, label = es_workload_by_lab), size = 3.5, fontface = "bold", vjust = -0.5) +
@@ -90,7 +91,7 @@ combined_plots
 
 
 # saving the plot as image png  
-ggsave("combined_plot.png", combined_plots, path = "../data/outputs/", width = 8, height= 8) 
+ggsave("combined_plot.png", combined_plots, path = "../data/outputs/", width = 11, height= 7) 
 
 
 
