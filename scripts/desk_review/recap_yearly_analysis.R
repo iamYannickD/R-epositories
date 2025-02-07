2780957a33d1059384c00b70d2ecc605988c390c
iamyannickd <yannickdoungmo@gmail.com>
2025-02-07 20:42:51 +0100

    Update recap_yearly_analysis.R

--- scripts/desk_review/recap_yearly_analysis.R
+++ scripts/desk_review/recap_yearly_analysis.R
@@ -6,7 +6,7 @@ library("pacman")
 p_load(tidyverse, gt, gtExtras)
 
 # load data
-es_sites <- read_csv("../data/data_dr/es_sites/ES_performance_from_2024-01-01_to_2024-12-31 Cumul_v3_.csv") 
+es_sites <- read_csv("../data/data_dr/es_sites/ES_performance_by Year_1.csv") 
 
 # Classification of countries based on their level of risk
 Very_high_risk <- c("Chad", "Democratic Republic of Congo", "Madagascar", "Mozambique", "Niger", "Nigeria")
@@ -42,76 +42,34 @@ risk_level_by_country <- tibble(
 
 recap_es_sites <- 
   es_sites |>
-  dplyr::select(Countryname = Country, EV_isolation_Rate_3m, EV_isolation_Rate_6m, EV_isolation_Rate_9m, EV_isolation_Rate_12m) |>
+  dplyr::select(Countryname = Country, "2020", "2021", "2022", "2023", "2024") |>
     left_join(y = risk_level_by_country, by = c("Countryname" = "country")) |>
     filter(!is.na(risk_level)) |>
     group_by(risk_level, Countryname) |>
     mutate(
-      country_3m = if_else(!is.na(EV_isolation_Rate_3m), 1, 0),
-      country_3m_50ev = if_else( (!is.na(EV_isolation_Rate_3m) & EV_isolation_Rate_3m  >= 0.5) , 1, 0),
-      country_3m_80ev = if_else( (!is.na(EV_isolation_Rate_3m) & EV_isolation_Rate_3m  >= 0.8), 1, 0),
-      
-      country_6m = if_else(!is.na(EV_isolation_Rate_6m), 1, 0),
-      country_6m_50ev = if_else( (!is.na(EV_isolation_Rate_6m) & EV_isolation_Rate_6m  >= 0.5) , 1, 0),
-      country_6m_80ev = if_else( (!is.na(EV_isolation_Rate_6m) & EV_isolation_Rate_6m  >= 0.8), 1, 0),
-      
-      country_9m = if_else(!is.na(EV_isolation_Rate_9m), 1, 0),
-      country_9m_50ev = if_else( (!is.na(EV_isolation_Rate_9m) & EV_isolation_Rate_9m  >= 0.5) , 1, 0),
-      country_9m_80ev = if_else( (!is.na(EV_isolation_Rate_9m) & EV_isolation_Rate_9m  >= 0.8), 1, 0),
-      
-      country_12m = if_else(!is.na(EV_isolation_Rate_12m), 1, 0),
-      country_12m_50ev = if_else( (!is.na(EV_isolation_Rate_12m) & EV_isolation_Rate_12m  >= 0.5) , 1, 0),
-      country_12m_80ev = if_else( (!is.na(EV_isolation_Rate_12m) & EV_isolation_Rate_12m  >= 0.8), 1, 0)
-    ) |>
-    summarise(
-      `%Q1countries >= 50` = 100 * sum(country_3m_50ev, na.rm = TRUE) / sum(country_3m, na.rm = TRUE),
-      `%Q2countries >= 50` = 100 * sum(country_6m_50ev, na.rm = TRUE) / sum(country_6m, na.rm = TRUE),
-      `%Q3countries >= 50` = 100 * sum(country_9m_50ev, na.rm = TRUE) / sum(country_9m, na.rm = TRUE),
-      `%Q4countries >= 50` = 100 * sum(country_12m_50ev, na.rm = TRUE) / sum(country_12m, na.rm = TRUE),
-      
-      `%Q1countries >= 80` = 100 * sum(country_3m_80ev, na.rm = TRUE) / sum(country_3m, na.rm = TRUE),
-      `%Q2countries >= 80` = 100 * sum(country_6m_80ev, na.rm = TRUE) / sum(country_6m, na.rm = TRUE),
-      `%Q3countries >= 80` = 100 * sum(country_9m_80ev, na.rm = TRUE) / sum(country_9m, na.rm = TRUE),
-      `%Q4countries >= 80` = 100 * sum(country_12m_80ev, na.rm = TRUE) / sum(country_12m, na.rm = TRUE)
-    ) |>
-    mutate(
-      #sparkline_50 = map2( `%Q1countries >= 50`, `%Q2countries >= 50`, ~ list(c(.x, .y))),
-          sparkline_50 = map2( `%Q1countries >= 50`, `%Q2countries >= 50`, `%Q3countries >= 50`, `%Q4countries >= 50`, ~ list(c(.x, .y))),
-          sparkline_80 = map2( `%Q1countries >= 80`, `%Q2countries >= 80`, `%Q3countries >= 80`, `%Q4countries >= 80`, ~ list(c(.x, .y)))
-          
-      #sparkline_80 = map2_dbl( `%Q1countries >= 80`, `%Q2countries >= 80`, ~ list(c(.x, .y)))
-      #sparkline_80 = pmap(list(`%Q1countries >= 80`, `%Q2countries >= 80`), ~ c(..1, ..2))
-          #sparkline_80 = pmap(list(`%Q1countries >= 80`, `%Q2countries >= 80`), `%Q3countries >= 80`, ~ c(..1, ..2))
-      #sparkline_50 = as.list(paste0( round(`%Q1countries >= 50`, digits = 0), ", ", round(`%Q2countries >= 50`, digits = 0))),
-      #sparkline_80 = as.list(paste0( round(`%Q1countries >= 80`, digits = 0), ", ", round(`%Q2countries >= 80`, digits = 0)))
+          sparkline_50 = map2( `2020`, `2021`, `2022`, `2023`, `2024`, ~ list(c(.x, .y))),
     ) |>
     arrange(risk_level, Countryname) |>
     gt(groupname_col = 'risk_level', rowname_col = 'Countryname') |>
     #gt_color_rows(2:6, palette = c("darkred", "red", "yellow", "green", "darkgreen")) |> # domain = c(0, 0.50), plt =  "RdYlGn"
     #edit some columns names
     cols_label(
-      "%Q1countries >= 50" = "Q1, 2024",
-      "%Q2countries >= 50" = "Q2, 2024",
-      "%Q3countries >= 50" = "Q3, 2024",
-      "%Q4countries >= 50" = "Q4, 2024",
-      "%Q1countries >= 80" = "Q1, 2024",
-      "%Q2countries >= 80" = "Q2, 2024",
-      "%Q3countries >= 80" = "Q3, 2024",
-      "%Q4countries >= 80" = "Q4, 2024",
+      "2020" = "2020",
+      "2021" = "2021",
+      "2022" = "2022",
+      "2023" = "2023",
+      "2024" = "2024",
       sparkline_50 = "EV Isolation 50% Trend",
-      sparkline_80 = "EV Isolation 80% Trend"
     ) |>
     # add percentage in cells
     fmt_number(
-      columns = c(`%Q1countries >= 50`, `%Q2countries >= 50`, `%Q3countries >= 50`, `%Q4countries >= 50`, 
-                  `%Q1countries >= 80`, `%Q2countries >= 80`, `%Q3countries >= 80`, `%Q4countries >= 80`),
+      columns = c(`2020`, `2021`, `2022`, `2023`, `2024`),
       decimals = 0,
       pattern = "{x} %"
     ) |> 
     summary_rows(
       groups = everything(),
-      columns = c(`%Q1countries >= 50`, `%Q2countries >= 50`, `%Q3countries >= 50`, `%Q4countries >= 50`, 
-                  `%Q1countries >= 80`, `%Q2countries >= 80`, `%Q3countries >= 80`, `%Q4countries >= 80`),
+      columns = c(`2020`, `2021`, `2022`, `2023`, `2024`),
       fns = list(
         "SUB TOTAL (MEAN)" = ~ mean(.x, na.rm = TRUE)
       ),
@@ -119,8 +77,7 @@ recap_es_sites <-
       decimals = 0
     ) |>
     grand_summary_rows(
-      columns = c(`%Q1countries >= 50`, `%Q2countries >= 50`, `%Q3countries >= 50`, `%Q4countries >= 50`, 
-                  `%Q1countries >= 80`, `%Q2countries >= 80`, `%Q3countries >= 80`, `%Q4countries >= 80`),
+      columns = c(`2020`, `2021`, `2022`, `2023`, `2024`),
       fns = list(
         "GRAND TOTAL (MEAN)" = ~ mean(.x, na.rm = TRUE)
       ),
@@ -154,13 +111,12 @@ recap_es_sites <-
       # ) |> 
       # Color the table cells by category defined
       data_color(
-        columns = c(`%Q1countries >= 50`, `%Q2countries >= 50`, `%Q3countries >= 50`, `%Q4countries >= 50`, 
-                    `%Q1countries >= 80`, `%Q2countries >= 80`, `%Q3countries >= 80`, `%Q4countries >= 80`),
+        columns = c(`2020`, `2021`, `2022`, `2023`, `2024`),
         fn = scales::col_bin(palette=colors, bins=breaks)
       ) |>
     # Add a nanoplot at the end of the 'group' in the table to show trends of ES Sites >= 50%
     gt::cols_nanoplot(
-      columns = contains(">= 50"),
+      columns = c(`2020`, `2021`, `2022`, `2023`, `2024`),
       autoscale = TRUE,
       autohide = FALSE,
       #plot_type = "bar",
@@ -181,26 +137,21 @@ recap_es_sites <-
     # ) |>
     #give a header to the table as well as a sub title
     tab_header(
-      title = md("**Summary of ES site sensitivity, Q1, Q2, Q3 & Q4, 2024**"),
+      title = md("**ES Country Performance from 2020 to 2024**"),
       subtitle = md("**Data source : AFRO ES Database**") ) |>
     #add the title that covers specific columns
     tab_spanner(
       label = md('**% of ES sites with ≥50% EV isolation**'),
-      columns = 3:6)  |>
-    tab_spanner(
-      label = md('**% of ES sites with ≥80% EV isolation**'),
-      columns = 8:11) |>
+      columns = 2:8)  |>
     #center the values in the defined columns
     cols_align(
       align = "center",
-      columns = c(`%Q1countries >= 50`, `%Q2countries >= 50`, `%Q3countries >= 50`, `%Q4countries >= 50`,
-                  `%Q1countries >= 80`, `%Q2countries >= 80`, `%Q3countries >= 80`, `%Q4countries >= 80`, nanoplots_50) #nanoplots_80)
+      columns = c(`2020`, `2021`, `2022`, `2023`, `2024`, nanoplots_50) #nanoplots_80)
     ) |>
     # Hide some unused columns
     cols_hide(
       columns = c(
-        sparkline_50,
-        sparkline_80
+        sparkline_50
       )
     ) |>
     gt_theme_guardian() |>
