f8f08021bb0eb50c6c9ea0ff57694182b4f3b2ea
iamyannickd <yannickdoungmo@gmail.com>
2025-02-08 15:00:51 +0100

    Update pvs_and_pending_desk review.R

--- scripts/desk_review/pvs_and_pending_desk review.R
+++ scripts/desk_review/pvs_and_pending_desk review.R
@@ -72,8 +72,8 @@ for (cntry in countries) {
                                             #xlim = c(NA, Inf), ylim = c(-Inf, Inf), clip = "ON",
                                             min.segment.length = 0.5) +
                             theme_bw() +
-                            theme(plot.title = element_text(hjust = 0.5)) + # Center ggplot title
-                            theme(legend.position = "bottom")
+                            theme(plot.title = element_text(hjust = 0.5)) #+ # Center ggplot title
+                            #theme(legend.position = "bottom")
                          
                          ggsave(paste0("../data/data_dr/outputs/Map_Virus/", cntry,".png"), plot1) #export population map + es sites
                         }
