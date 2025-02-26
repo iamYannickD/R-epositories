b28f51cab1c451891ce762418d0c593b0b4837e2
iamyannickd <yannickdoungmo@gmail.com>
2025-02-26 15:15:58 +0100

    DQC of Weekly Lab DBs
    Scripts for weekly DQC of Lab databases, focusing on one or multiple labs and the countries they serve. These scripts flag changes, including additions, deletions, and updated results.

---
+++ scripts/db_assesment/Slide 2 - Summary ES KPIs.R
@@ -0,0 +1,238 @@
+# Check if the package pacman  is installed
+if (!require("pacman")) {install.packages("pacman")} 
+library("pacman")
+
+# Load packages =====
+#RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
+p_load(tidyverse, RODBC,gt, gtExtras)
+
+#Give the path to the ES database
+Specify_the_period <- "WEEK 1 - 28, 2024"
+path_ES_2024 = "../data/dbs/ES_160724.mdb"
+labname <- "CAE" # Replace with the actual labname you want to filter by
+
+# Connect to the Microsoft Access database ====
+ESdb2024 <- DBI::dbConnect(odbc::odbc(), 
+                           .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                              DBQ=", path_ES_2024))
+# load data in R =====
+# Retrieve all data from the AFP database
+EStables2024 <- DBI::dbGetQuery(ESdb2024, "SELECT * FROM Environmental ORDER BY IDNumber;", stringsAsFactors = FALSE) |>
+  as_tibble() |>
+  mutate(Labname = str_replace_all(Labname, c("ENTEBBE" = "UGA", "GHANA" = "GHA", "INRB" = "RDC", "IPD SEN" = "SEN",
+                                              "IPM, MAD" = "MAD", "IPM,MAD" = "MAD", "KEMRI" = "KEN", "IBD, Nigeria" = "IBD",
+                                              "MDG, Nigeria" = "MDG", "ZAM UTH" = "ZAM", "ZAM-UTH" = "ZAM")),
+         date_result_to_lab = coalesce(Dateresultstolab, Datefinalcultureresult),
+         date_itd_result = coalesce(DateFinalCombinedResult, DatefinalResultReported)
+  )
+
+#Specify_the_period <- paste0("WEEK 1 - ", (epiweek(as.Date(ymd_hms(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
+
+# Analysis of databases =====
+EStables_gt <- 
+  EStables2024 |>
+  mutate(Labname = str_replace_all(Labname, "ESWATINI", "SOA" )) |>
+  filter(Labname == labname) |>
+  group_by(Countrycode) |>
+  summarize(
+    nb_workload_by_lab = n(),
+    nb_Sample_good_cond = sum(Samplecondition == "1-Good", na.rm = TRUE),
+    Prop_sample_good_cond = round(nb_Sample_good_cond / nb_workload_by_lab * 100, 0),
+    
+    nb_culture_results = sum(!is.na(Finalcellcultureresult) & !is.nan(Finalcellcultureresult) & !is.null(Finalcellcultureresult), na.rm = TRUE),
+    nb_culture_results_14days = sum(!is.na(Finalcellcultureresult) & !is.nan(Finalcellcultureresult) & !is.null(Finalcellcultureresult) &
+                                   (Datefinalcultureresult - Datesampleinlab) < 15 & 
+                                   (Datefinalcultureresult - Datesampleinlab) >= 0, na.rm = TRUE),
+    Prop_culture_results_14days = round(nb_culture_results_14days / nb_culture_results * 100, 0),
+    
+    nb_ITD_results = sum(str_detect(Finalcellcultureresult, "^1") | str_detect(Finalcellcultureresult, "^4"), na.rm = TRUE),
+    nb_ITD_results_7days = sum((str_detect(Finalcellcultureresult, "^1") | str_detect(Finalcellcultureresult, "^4")) &
+                              (as.Date(date_itd_result) - as.Date(date_result_to_lab)) < 8 & 
+                              (as.Date(date_itd_result) - as.Date(date_result_to_lab)) >= 0, na.rm = TRUE),
+    Prop_ITD_7days = round(nb_ITD_results_7days / nb_ITD_results * 100, 0),
+    
+    nb_ITD_results_21days = sum( (str_detect(Finalcellcultureresult, "^1") | str_detect(Finalcellcultureresult, "^4")) & 
+                                !is.na(FinalcombinedrRTPCRresults) & (DateFinalCombinedResult - Datesampleinlab) < 22 & 
+                                (DateFinalCombinedResult - Datesampleinlab) >= 0, na.rm = TRUE),
+    Prop_ITD_21days = round(nb_ITD_results_21days / nb_ITD_results * 100, 0)
+  ) |>
+  select(Countrycode, nb_workload_by_lab, nb_Sample_good_cond, Prop_sample_good_cond, nb_culture_results, nb_culture_results_14days,
+         Prop_culture_results_14days, nb_ITD_results, nb_ITD_results_7days, Prop_ITD_7days, nb_ITD_results_21days, Prop_ITD_21days) |>
+  gt() |>
+  #edit some columns names
+  cols_label(
+    "nb_workload_by_lab" = "# of Stool specimens",
+    "nb_Sample_good_cond" = "# samples good conditions",
+    "nb_culture_results" = "# Culture Results",
+    "nb_culture_results_14days" = "# of Culture results in 14 days", 
+    "nb_ITD_results" = "# ITD Results",
+    "nb_ITD_results_7days" = "ITD Results in 7 days",
+    "nb_ITD_results_21days" = "ITD results in 21 days",
+    "Prop_sample_good_cond" = "Samples in Good Condition",
+    "Prop_culture_results_14days" = "PV Isolation Results on Time",
+    "Prop_ITD_7days" = "ITD Results in 7 days of receipt of Isolate",
+    "Prop_ITD_21days" = "Stool speciments with Final lab results availaible in 21 days of receipt"
+  ) |> 
+  # insert a summary for proportions
+  grand_summary_rows(
+    columns = starts_with("Prop"),
+    fns = list(
+      "AVG" = ~ median(.x, na.rm = TRUE)
+    ) )  |>
+  grand_summary_rows(
+    columns = starts_with("nb_"),
+    fns = list(
+      "TOTAL" = ~ sum(.x, na.rm = TRUE)
+    ) ) |>
+  #center the values in the defined columns
+  cols_align(
+    align = "center",
+    columns = c(1:11)
+  ) |>
+  #give a header to the table as well as a sub title
+  tab_header(
+    title = md(paste0("**ES : SUMMARY OF AFRO LABORATORY KEY PERFORMANCE INDICATORS (KPIs) in**", " ", labname)),
+    subtitle = md(paste0("**",Specify_the_period,"**")) ) |>
+  # add percentage in cells
+  fmt_number(
+    columns = c(`Prop_sample_good_cond`, `Prop_culture_results_14days`,
+                `Prop_ITD_7days`, `Prop_ITD_21days`),
+    decimals = 0,
+    pattern = "{x} %"
+  ) |>
+  sub_missing(
+    columns = 2:11,
+    rows = everything(),
+    missing_text = "-"
+    #missing_text = "---"
+  ) |>
+  #color the table based on the values in those cells
+  # For sample conditions ====
+tab_style(
+  style = cell_fill(color = "#00B050"),
+  locations = cells_body(
+    columns = Prop_sample_good_cond,
+    rows = Prop_sample_good_cond >= 80)
+)  |>
+  tab_style(
+    style = cell_fill(color = "yellow"),
+    locations = cells_body(
+      columns = Prop_sample_good_cond,
+      rows = Prop_sample_good_cond < 80)
+  ) |> 
+  tab_style(
+    style = cell_fill(color = "#00B050"),
+    locations = cells_body(
+      columns = Prop_sample_good_cond,
+      rows = Prop_sample_good_cond == 100)
+  ) |>
+  # for pv isolation time 14 days
+  tab_style(
+    style = cell_fill(color = "#00B050"),
+    locations = cells_body(
+      columns = Prop_culture_results_14days,
+      rows = Prop_culture_results_14days >= 80)
+  )  |>
+  tab_style(
+    style = cell_fill(color = "yellow"),
+    locations = cells_body(
+      columns = Prop_culture_results_14days,
+      rows = Prop_culture_results_14days < 80)
+  ) |> 
+  tab_style(
+    style = cell_fill(color = "#00B050"),
+    locations = cells_body(
+      columns = Prop_culture_results_14days,
+      rows = Prop_culture_results_14days == 100)
+  ) |>
+  # for ITD 7 days
+  tab_style(
+    style = cell_fill(color = "#00B050"),
+    locations = cells_body(
+      columns = Prop_ITD_7days,
+      rows = Prop_ITD_7days >= 80)
+  )  |>
+  tab_style(
+    style = cell_fill(color = "yellow"),
+    locations = cells_body(
+      columns = Prop_ITD_7days,
+      rows = Prop_ITD_7days < 80)
+  ) |>
+  tab_style(
+    style = cell_fill(color = "#00B050"),
+    locations = cells_body(
+      columns = Prop_culture_results_14days,
+      rows = Prop_culture_results_14days == 100)
+  ) |>
+  # for ITD 21 days
+  tab_style(
+    style = cell_fill(color = "#00B050"),
+    locations = cells_body(
+      columns = Prop_ITD_21days,
+      rows = Prop_ITD_21days >= 80)
+  )  |>
+  tab_style(
+    style = cell_fill(color = "yellow"),
+    locations = cells_body(
+      columns = Prop_ITD_21days,
+      rows = Prop_ITD_21days < 80)
+  ) |>
+  tab_style(
+    style = cell_fill(color = "#00B050"),
+    locations = cells_body(
+      columns = Prop_ITD_21days,
+      rows = Prop_ITD_21days == 100)
+  ) |>
+  # for no data
+  tab_style(
+    style = cell_fill(color = "gray"),
+    locations = cells_body(
+      columns = Prop_ITD_21days,
+      rows = is.na(Prop_ITD_21days))
+  ) |>
+  tab_style(
+    style = cell_fill(color = "gray"),
+    locations = cells_body(
+      columns = Prop_ITD_7days,
+      rows = is.na(Prop_ITD_7days))
+  ) |>
+  
+  # Color in gray the table and beautify the formating
+  opt_stylize(style = 6, color = 'gray') |>
+  #call that theme
+  #other themes gt_theme_excel()  |>gt_theme_pff() |>
+  gt_theme_excel() |>
+  
+  opt_align_table_header(align = "center") |>
+  #reshape the table
+  tab_options(
+    data_row.padding = px(2),
+    summary_row.padding = px(3), # A bit more padding for summaries
+    row_group.padding = px(4)    # And even more for our groups
+  ) |>
+  tab_style(
+    #style = cell_text(weight = "bold"),
+    # Color each country in the group
+    style = cell_fill(color = "lightgray"),
+    locations = cells_row_groups(groups = everything()) 
+  ) |>
+  opt_css(
+    css = "
+    .cell-output-display {
+      overflow-x: unset !important;
+    }
+    div#two {
+      overflow-x: unset !important;
+      overflow-y: unset !important;
+    }
+    #two .gt_col_heading {
+      position: sticky !important;
+      top: 0 !important;
+    }
+    "
+  )
+
+EStables_gt
+
+# export my table
+gtsave(EStables_gt, "../data/outputs_lab_ass/EStables.html")
---
+++ scripts/db_assesment/Slide 3 - AFP Primary Isolation KPIs.R
@@ -0,0 +1,248 @@
+# Check if the package pacman  is installed
+if (!require("pacman")) {install.packages("pacman")} 
+library("pacman")
+
+# Load packages =====
+#RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
+p_load(tidyverse, RODBC,gt, gtExtras, webshot, openxlsx)
+
+#Give the path to the AFP database
+path_AFP <- "../data/dbs/AFP_160724.mdb" 
+labname <- "CAE" # Replace with the actual labname you want to filter by
+#dateDBreceived <- "2024-06-18 14:24:05"
+
+# Connect to the Microsoft Access database =====
+AFPdb <- DBI::dbConnect(odbc::odbc(), 
+                        .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                              DBQ=", path_AFP))
+# load data in R =====
+# Retrieve all data from the AFP database
+AFPtables <- DBI::dbGetQuery(AFPdb, "SELECT * FROM POLIOLAB ORDER BY LabName, EpidNumber;", stringsAsFactors = FALSE) |>
+  tibble() |>
+  # select samples collected in 2024 only
+  filter(substr(ICLabID, start = 5, stop = 6) == 24 ) 
+
+Specify_the_period <- paste0("WEEK 1 - ", 
+                             (epiweek(as.Date(ymd(AFPtables$DateUpdated))) - 1) |> unique(), ", 2024")
+
+# Analysis of databases =====
+AFPkpis <-
+  AFPtables |>
+  filter(LabName == labname) |>
+  mutate( FinalCellCultureResult = str_replace_all(FinalCellCultureResult, "Supected", "Suspected"),
+          CountryCode = substr(EpidNumber, start = 1, stop = 3), .after = LabName) |>
+  select(CountryCode, ICLabID, DateStoolReceivedinLab, FinalCellCultureResult, DateFinalCellCultureResults, DateUpdated) |>
+  distinct(ICLabID, .keep_all = "TRUE") |>
+  group_by(CountryCode) |>
+  mutate(is_result = if_else(!is.na(FinalCellCultureResult), 1, 0),
+         is_pv_positive = if_else((FinalCellCultureResult == "1-Suspected Poliovirus"), 1, 0),
+         is_pv_positive_and_npent = if_else((FinalCellCultureResult == "4-Suspected Poliovirus + NPENT"), 1, 0),
+         is_pv_negative = if_else((FinalCellCultureResult == "2-Negative"), 1, 0),
+         is_npent = if_else((FinalCellCultureResult == "3-NPENT"), 1, 0),
+         
+         # cell culture < 14 days
+         time_culture_results = as.numeric(difftime(DateFinalCellCultureResults, DateStoolReceivedinLab, units = "days")),
+         is_culture_results_14days = if_else( (!is.na(FinalCellCultureResult) & time_culture_results < 15 & time_culture_results > 0), 1, 0),
+         
+         culture_results = sum(is_result),
+         culture_results_14days = sum(is_culture_results_14days),
+         
+         # use a generic data based on the date the database was received
+         #DateUpdated = if_else(is.na(DateUpdated), as.POSIXct(dateDBreceived), as.Date(ymd_hms(DateUpdated))),
+         DateUpdated = as.Date(ymd(DateUpdated)),
+         
+         DateStoolReceivedinLab = as.Date(ymd(DateStoolReceivedinLab)),
+         #DateUpdated = as.Date(ymd_hms(DateUpdated)),
+         days_to_results = as.numeric(difftime(DateUpdated, DateStoolReceivedinLab, units = "days")),
+         is_pending_culture_results = if_else(is.na(FinalCellCultureResult) & days_to_results >= 15, 1, 0),
+         is_pending_culture_15_30 = if_else(is.na(FinalCellCultureResult) & (days_to_results >= 15 & days_to_results < 30), 1, 0),
+         is_pending_culture_30_60 = if_else(is.na(FinalCellCultureResult) & (days_to_results >= 30 & days_to_results < 60), 1, 0),
+         is_pending_culture_more_60 = if_else(is.na(FinalCellCultureResult) & days_to_results >= 60, 1, 0)
+  ) |> 
+  summarise(
+    nb_samples_with_results = sum(is_result, na.rm = TRUE),
+    
+    nb_culture_results_14days = sum(is_culture_results_14days, na.rm = TRUE),
+    Prop_culture_results_14days = 100 * nb_culture_results_14days / nb_samples_with_results,
+    
+    nb_pv_positive = sum(is_pv_positive, na.rm = TRUE),
+    prop_pv_positive = 100 * nb_pv_positive / nb_samples_with_results,
+    
+    nb_pv_positive_and_npent = sum(is_pv_positive_and_npent, na.rm = TRUE),
+    prop_pv_positive_and_npent = 100 * nb_pv_positive_and_npent / nb_samples_with_results,
+    
+    nb_npent = sum(is_npent, na.rm = TRUE), 
+    prop_npent = 100 * nb_npent / nb_samples_with_results,
+    
+    nb_pv_negative = sum(is_pv_negative, na.rm = TRUE),
+    prop_negative = 100 * nb_pv_negative / nb_samples_with_results,
+    
+    nb_pending_culture_results = sum(is_pending_culture_results),
+    nb_pending_culture_15_30 = sum(is_pending_culture_15_30),
+    nb_pending_culture_30_60 = sum(is_pending_culture_30_60),
+    nb_pending_culture_more_60 = sum(is_pending_culture_more_60)
+  ) |>
+  ungroup() |>
+  gt() |>
+  #edit some columns names
+  cols_label(
+    "nb_samples_with_results" = "# of Specimens with Results",
+    "nb_culture_results_14days" = "# of culture results in 14 days",
+    "Prop_culture_results_14days" = "Prop cell cultures results 14 days",
+    "nb_pv_positive" = "Culture +ve for PV",
+    "prop_pv_positive" = "Prop Culture +ve for PV",
+    "nb_pv_positive_and_npent" = "Culture +ve for PV & NPEV",
+    "prop_pv_positive_and_npent" = "Prop Culture +ve for PV & NPEV",
+    "nb_npent" = "NPEV",
+    "prop_npent" = "Prop NPEV",
+    "nb_pv_negative" = "Negative",
+    "prop_negative" = "Prop Negative",
+    "nb_pending_culture_results" = "Total Pending",
+    "nb_pending_culture_15_30" = "15 - 30 days",
+    "nb_pending_culture_30_60" = "31 - 60 days",
+    "nb_pending_culture_more_60" = "> 60 days"
+  ) |>
+  # insert a summary for proportions
+  grand_summary_rows(
+    columns = starts_with("Prop"),
+    #missing_text = "-",
+    fns = list(
+      "AVG" = ~ median(.x, na.rm = TRUE)
+    ) )  |>
+  # fmt_percent(
+  #   columns = starts_with("Prop"),
+  #   rows = grand_summary_rows,
+  #   decimals = 1 ) 
+  grand_summary_rows(
+    columns = starts_with("nb_"),
+    #missing_text = "-",
+    fns = list(
+      "TOTAL" = ~ sum(.x, na.rm = TRUE)
+    ) )  |>
+  # customize the summary line
+  tab_style(
+    style = list(
+      cell_text(weight = "bold", size = px(14))
+    ),
+    locations = cells_grand_summary()
+  )  |>
+  #center the values in the defined columns
+  cols_align(
+    align = "center",
+    columns = c(2:16)
+  ) |>
+  tab_spanner(
+    label = md('**Culture +ve for PV**'),
+    columns = 5:6) |>
+  tab_spanner(
+    label = md('**Culture +ve for PV & NPEV**'),
+    columns = 7:8) |>
+  tab_spanner(
+    label = md('**NPEV**'),
+    columns = 9:10) |>
+  tab_spanner(
+    label = md('**Negative**'),
+    columns = 11:12) |>
+  #add the title that covers the columns in the 3th and 10th row
+  tab_spanner(
+    label = md('**PRIMARY VIRUS ISOLATION RESULTS**'),
+    columns = 3:12) |>
+  #add the title that covers the columns in the 7th and 8th row
+  tab_spanner(
+    label = md('**Pending Samples**'),
+    columns = 13:16) |>
+  #give a header to the table as well as a sub title
+  tab_header(
+    title = md(paste0("**AFP : Timeliness and Results of Primary Isolation (14 days)** ")),
+    subtitle = md(paste0("**",Specify_the_period,"**")) ) |>
+  sub_missing(
+    columns = 2:16,
+    rows = everything(),
+    missing_text = 0
+    #missing_text = "---"
+  ) |>
+  # add percentage in cells
+  fmt_number(
+    columns = starts_with("nb_"),
+    decimals = 0,
+    pattern = "{x}"
+  ) |>
+  fmt_number(
+    columns = starts_with("Prop"),
+    decimals = 1,
+    pattern = "{x} %"
+  ) |>
+  #color the table based on the values in those cells
+  # For sample conditions ====
+tab_style(
+  style = cell_fill(color = "pink"),
+  locations = cells_body(
+    columns = nb_pending_culture_results,
+    rows = nb_pending_culture_results >= 15)
+)  |>
+  tab_style(
+    style = cell_fill(color = "red2"),
+    locations = cells_body(
+      columns = nb_pending_culture_results,
+      rows = nb_pending_culture_results >= 31)
+  ) |> 
+  tab_style(
+    style = cell_fill(color = "red4"),
+    locations = cells_body(
+      columns = nb_pending_culture_results,
+      rows = nb_pending_culture_results >= 61)
+  ) |>
+  data_color(
+    #method = "numeric",
+    columns = nb_pending_culture_results,
+    rows = nb_pending_culture_results >= 1,
+    palette = "ggsci::red_material"
+  )  |>
+  # Color in gray the table and beautify the formating
+  #opt_stylize(style = 6, color = 'gray') |> #remove style on lines
+  gt_theme_538() |>
+  
+  opt_align_table_header(align = "center") |>
+  #reshape the table
+  tab_options(
+    data_row.padding = px(2),
+    summary_row.padding = px(3), # A bit more padding for summaries
+    row_group.padding = px(4)    # And even more for our groups
+  ) |>
+  tab_style(
+    #style = cell_text(weight = "bold"),
+    # Color each country in the group
+    style = cell_fill(color = "lightgray"),
+    locations = cells_row_groups(groups = everything()) 
+  ) |>
+  opt_css(
+    css = "
+    .cell-output-display {
+      overflow-x: unset !important;
+    }
+    div#two {
+      overflow-x: unset !important;
+      overflow-y: unset !important;
+    }
+    #two .gt_col_heading {
+      position: sticky !important;
+      top: 0 !important;
+    }
+    "
+  )
+
+AFPkpis
+
+# export my table
+gtsave(AFPkpis, "../data/outputs_lab_ass/AFPKpis.html")
+# Convert HTML to PNG
+webshot::webshot("../data/outputs_lab_ass//AFPtables.html", "../data/output/AFPKpis.png")
+
+# convert html to excel file
+#write.xlsx(AFPkpis_df, "output/AFPKpis.xlsx")
+
+
+
+
+
+
