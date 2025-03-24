9f67488fe3f9c2ea74cbbaccf4247501316cb53c
iamyannickd <yannickdoungmo@gmail.com>
2025-03-24 13:03:51 +0100

    Update Slide 13 ind1.1 - Timeliness AFP Results in 35 days by Country (with Full Lab Capacity).R

--- scripts/pln_bulletin/GPSAP 2025-2026 Scripts/Slide 13 ind1.1 - Timeliness AFP Results in 35 days by Country (with Full Lab Capacity).R
+++ scripts/pln_bulletin/GPSAP 2025-2026 Scripts/Slide 13 ind1.1 - Timeliness AFP Results in 35 days by Country (with Full Lab Capacity).R
@@ -4,60 +4,90 @@ library("pacman")
 
 # Load packages =====
 #RODBC to be able to work with microsoft access databases, allowing R to connect to Open Database Connectivity (ODBC) APIs
-p_load(tidyverse, RODBC,gt, gtExtras, webshot)
+p_load(tidyverse, RODBC, gt, gtExtras)
 
-#Give the path to the Sequencing results file
 seq_result_AFP <- read.csv("../data/data/All virus sequencing results.csv") |>
-                          mutate(DATE_COLL = dmy(ONSET..COLLECTION)) |>
-                          filter(SOURCE != "ENV" & (today() - DATE_COLL < 366)) |>  # (year(DATE_COLL) > 2024 ) or #%in% c(2024, 2025)
-                          mutate(DATE_RECEIVED = dmy(DATE.RECEIVED)) |>
-                          mutate(TAT = difftime(DATE_RECEIVED, DATE_COLL)) |> tibble()
+  mutate(DATE_COLL = dmy(ONSET..COLLECTION)) |>
+  filter(SOURCE != "ENV" & (today() - DATE_COLL < 366)) |>
+  mutate(DATE_RECEIVED = dmy(DATE.RECEIVED)) |>
+  mutate(TAT = difftime(DATE_RECEIVED, DATE_COLL), 
+         TAT_days = as.numeric(TAT)) |>
+  tibble()
 
-
-# Analysis of databases =====
-Sequencing.results.countries.with.labs <- 
-  seq_result_AFP |>
+# Préparation du jeu de données avec IST et CountryCode
+data_AFP <- seq_result_AFP |>
   mutate(
-    CountryCode = substr(EPID.NUMBER, start = 1, stop = 3), .before = COUNTRY) |>
-    mutate(IST = case_when(CountryCode %in% c("ALG", "BEN", "BFA", "CIV", "GAM", "GHA", "GUB", "GUI", "LIB", "MAI", "MAU",
-                                            "NIE", "NIG", "SEN", "SIL",  "TOG" ) ~ "WEST",
-                         CountryCode %in% c( "ANG", "CAE", "CAF", "CHA",  "EQG", "GAB", "CNG", "RDC") ~ "CENTRAL",
-                         CountryCode %in% c( "BOT", "BUU", "COM", "ETH", "KEN", "LES", "MAD", "MAL", "MOZ", "NAM", "RSS", "RWA",
-                                              "SOA", "SWZ", "TAN", "UGA", "ZAM", "ZIM") ~ "ESA"), .before = CountryCode) |>
-  select(IST, CountryCode, DATE_COLL, DATE_RECEIVED, TAT) |>
-  group_by(IST, CountryCode) |>
-  filter( (CountryCode %in% c("GHA", "SOA")) ) |> # "UGA", "NIE"
-  mutate(TAT_days = as.numeric(TAT)) |>
-  ggplot(aes(x = TAT_days)) +
-  geom_bar(aes(fill = TAT_days <= 35), color = "black", width = 1.5) +
-  scale_fill_manual(values = c("TRUE" = "green", "FALSE" = "red"), 
-                    labels = c("TRUE" = "<= 35 jours", "FALSE" = "> 35 jours")) +
-  geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5, size = 5) +
-  geom_vline(xintercept = 35, linetype = "dashed", color = "red", linewidth = 1) +
-  annotate("text", x = 35, y = Inf, label = "(35 days)", vjust = 2, hjust = -0.1, color = "red", size = 5) +
-  labs(
-    title = " ",
-    x = "Turnaround Time (in Days)",
-    y = "Number of Samples with Sequencing Results",
-    fill = "TAT (in Days)"
-  ) +
-  scale_x_continuous(
-    breaks = c(0, 15, 30, 45, 60),  # Graduations de l'axe X
-    limits = c(0, 60)  # Définir les limites de l'axe X
-  ) +
-  scale_y_continuous(
-    breaks = seq(0, 5, by = 1)
-  ) +
-  theme_minimal()
+    CountryCode = substr(EPID.NUMBER, start = 1, stop = 3),
+    .before = COUNTRY
+  ) |>
+  mutate(
+    IST = case_when(
+      CountryCode %in% c("ALG", "BEN", "BFA", "CIV", "GAM", "GHA", "GUB", "GUI", "LIB", "MAI", "MAU",
+                         "NIE", "NIG", "SEN", "SIL", "TOG") ~ "WEST",
+      CountryCode %in% c("ANG", "CAE", "CAF", "CHA", "EQG", "GAB", "CNG", "RDC") ~ "CENTRAL",
+      CountryCode %in% c("BOT", "BUU", "COM", "ETH", "KEN", "LES", "MAD", "MAL", "MOZ", "NAM", "RSS", "RWA",
+                         "SOA", "SWZ", "TAN", "UGA", "ZAM", "ZIM") ~ "ESA"
+    ),
+    .before = CountryCode
+  ) |>
+  filter(CountryCode %in% c("GHA", "SOA"))
+
+# Calcul des résumés par CountryCode pour l'annotation
+summary_data <- data_AFP |>
+  group_by(CountryCode) |>
+            summarise(
+              count_low = sum(TAT_days <= 35, na.rm = TRUE),
+              count_high = sum(TAT_days > 35, na.rm = TRUE),
+              total = n()
+            ) |>
+            mutate(
+              annotation = paste0("Total Samples with Seq Results: ", total, "\n TurnAround Time <= 35 days: ", count_low, " \n TurnAround Time > 35 days: ", count_high, 
+                                  "  \n Prop (<=35): ", round((count_low/total)*100, 1), "%  ")
+            )
+
+# Création du graphique
+Sequencing.results.countries.with.labs <- data_AFP |>
+            ggplot(aes(x = TAT_days)) +
+            geom_bar(aes(fill = TAT_days <= 35), color = "black", width = 1.5) +
+            scale_fill_manual(
+              values = c("TRUE" = "green", "FALSE" = "red"), 
+              labels = c("TRUE" = "<= 35 jours", "FALSE" = "> 35 jours")
+            ) +
+            geom_vline(xintercept = 35, linetype = "dashed", color = "red", linewidth = 1) +
+            annotate("text", x = 35, y = Inf, label = "(35 days)", 
+                     vjust = 2, hjust = -0.1, color = "red", size = 5) +
+            facet_wrap(vars(CountryCode), ncol = 2, scales = "free") +
+            labs(
+              title = "Turnaround Time (TAT)",
+              x = "Turnaround Time (in days)",
+              y = "Number of Samples with Sequencing Results",
+              fill = "TAT (in days)"
+            ) +
+            scale_x_continuous(
+              breaks = seq(0, max(data_AFP$TAT_days, na.rm = TRUE) + 1, by = 15),
+              limits = c(0, max(data_AFP$TAT_days, na.rm = TRUE) + 10)
+            ) +
+            #scale_y_continuous(
+            #  breaks = seq(0, 5, by = 1)
+            #) +
+            theme_minimal()
+
+# Ajout des annotations par facet
+Sequencing.results.countries.with.labs <- Sequencing.results.countries.with.labs + geom_label(
+            data = summary_data,
+            mapping = aes(x = Inf, y = Inf, label = annotation),
+            hjust = 1.1, vjust = 1.1,
+            size = 4,
+            fill = "white", #color = "black",
+            inherit.aes = FALSE,
+            #parse = TRUE
+            label.padding = unit(0.3, "lines"),
+            label.r = unit(0.2, "lines"))
 
 
 Sequencing.results.countries.with.labs
 
-
-# saving the plot as image png  
 ggsave("AFP_Countries_under_35_plot.png", Sequencing.results.countries.with.labs, path = "../data/outputs/", width = 13, height= 6) 
-
-
 
 
 
